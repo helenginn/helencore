fbb17092fa05555db84bdf4a0b71369bd4d86a80
Helen Ginn <github@hginn.co.uk>
2021-08-03 19:52:51 +0200

    Picky

--- hcsrc/FileReader.cpp
+++ hcsrc/FileReader.cpp
@@ -19,7 +19,8 @@
 
 std::string FileReader::outputDir;
 
-std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) 
+{
 	std::stringstream ss(s);
 	std::string item;
 	while (std::getline(ss, item, delim)) {
